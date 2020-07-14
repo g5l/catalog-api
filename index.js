d6937ad6fe619df458ca14fed3200c89d188bbd3
Gabriel Debona <gabriel.debona@gmail.com>
2020-07-14 14:11:38 -0300

    Add eslint

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const logger = require('./logger/logger');
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 
 app.use(helmet());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('combined'));
 
-const db = require("./src/models");
-const apiUser = require("./src/routes/user");
+const db = require('./src/models');
+const apiUser = require('./src/routes/user');
+
 apiUser(app, db);
 
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 app.listen(PORT, () => {
   logger.info('listening on port 3030');
 });
