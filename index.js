4bf26ac821b50aa8bc56b3674b64b6b817478a5b
Gabriel Debona <gabriel.debona@gmail.com>
2020-08-12 22:20:17 -0300

    Add header to fix cors error

--- index.js
+++ index.js
@@ -21,6 +21,14 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('combined'));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 const db = require('./src/models');
 const apiAuth = require('./src/auth');
 const apiCatalogUser = require('./src/routes/catalogUser');
