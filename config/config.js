f0db0be00cba1e5296bd5f55a15bc39b93b785a6
Gabriel Debona <gabriel.debona@gmail.com>
2020-11-12 16:38:21 -0300

    Change development config

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
 module.exports = {
   development: {
-    username: 'root',
-    password: '',
-    database: 'catalog_development',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     operatorsAliases: false,
   },
